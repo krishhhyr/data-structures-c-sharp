94bc0e4352670dacd78a0a4cd945ece3a4059006
Krishna R <rajamannar@hotmail.com>
2023-03-03 21:25:50 +0000

    Array Declaration added

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Single-Dimensional_Arrays
+            //Array Declaration
+
+            //Implictly Typed Array
+            string[] words = { "House", "Road", "Mouse", "Sleep", "Chicken" };
+
+            //Declaration by element type
+            int[] scores = new int[10]; 
+            scores[0] = 0;
+            scores[1] = 1;
+            scores[2] = 2;
+
         }
     }
 }
