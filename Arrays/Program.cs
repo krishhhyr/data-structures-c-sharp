552f0ce10f0a9838784b2e8a2239dcc2fa971bfc
Krishna R <rajamannar@hotmail.com>
2023-03-05 18:16:58 +0000

    Finished 1D arrays and started multidimensional arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace Arrays
 {
+    //Test
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region Single-Dimensional_Arrays
+            #region Single-Dimensional-Arrays
             //Array Declaration
 
             //Implictly Typed Array
@@ -15,6 +16,39 @@
             scores[0] = 0;
             scores[1] = 1;
             scores[2] = 2;
+
+            //Array User Input
+            string[] students = new string[10];
+            for (int k = 0; k < students.Length; k++) 
+            {
+                //Defensive Programming
+                string? input = Console.ReadLine();
+                if(!string.IsNullOrEmpty(input))
+                    students[k] = input;
+            }
+
+            //Ascending Array Output
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+            //Descending Array Ouput
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(words[i]);
+            }
+            #endregion
+            #region MultiDimensional-Arrays
+            //Array Declaration
+
+            //Two-dimensional array
+            //Four rows and two columns
+            int[,] table = new int[4, 2];
+
+            //Three-dimensional array 
+            //Two arrays which both have three rows and four columns
+            int[,,] table3D = new int[2, 3, 4];
             #endregion
 
         }
