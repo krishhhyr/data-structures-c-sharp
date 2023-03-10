f65695c805af50c9349b0716964906b668c9b92f
Krishna R <rajamannar@hotmail.com>
2023-03-10 11:55:50 +0000

    started implementing stacks

---
+++ Stacks/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Stacks
+{
+    internal class Program
+    {
+        //Declares the stack via an array
+        static int[] stack = new int[5];
+        //Used to check if the stack is empty or not
+        static int top = -1;
+        //Checks the number of elements in the stack
+        static int maxLength = stack.Length;
+        static void Main(string[] args)
+        {
+            
+        }
+        #region Using-Arrays
+
+        static void Pop() 
+        {
+            
+        }
+
+        #endregion
+    }
+}
