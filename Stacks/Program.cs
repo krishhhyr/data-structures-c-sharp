cd834db5232a12075715fe775d592de06500adba
Krishna R <rajamannar@hotmail.com>
2023-03-12 19:21:19 +0000

    added push and pop to the stack

--- Stacks/Program.cs
+++ Stacks/Program.cs
@@ -2,11 +2,11 @@
 {
     internal class Program
     {
-        //Declares the stack via an array
+        // Declares the stack via an array.
         static int[] stack = new int[5];
-        //Used to check if the stack is empty or not
+        // Used to check if the stack is empty or not.
         static int top = -1;
-        //Checks the number of elements in the stack
+        // Checks the number of elements in the stack.
         static int maxLength = stack.Length;
         static void Main(string[] args)
         {
@@ -14,10 +14,33 @@
         }
         #region Using-Arrays
 
+        // Checks if the stack is empty.
+        // If not, it decrements the counter "top"
         static void Pop() 
         {
-            
+            if (top != -1)
+            {
+                top--;
+            }
+            else 
+            {
+                Console.WriteLine("Stack is empty");
+            }
         }
+
+        static void Push(int item) 
+        {
+            if (top < maxLength - 1)
+            {
+                top += 1;
+                stack[top] = item;
+            }
+            else 
+            {
+                Console.WriteLine("Stack is full");
+            }
+        }
+
 
         #endregion
     }
