7a6a6503fafa9e67c1449900eb09dfa05bc66180
Krishna R <rajamannar@hotmail.com>
2023-03-23 16:37:02 +0000

    added dictionaries

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> CapitialsofCountries = new Dictionary<string, string>();
+            CapitialsofCountries.Add("England", "London");
+            CapitialsofCountries.Add("France", "Paris");
+            CapitialsofCountries.Add("Germany", "Berlin");
+            CapitialsofCountries.Add("USA", "Washington DC");
+            CapitialsofCountries.Add("Canada", "Ottawa");
+
+            if (CapitialsofCountries.ContainsKey("England")) 
+            {
+                Console.WriteLine(CapitialsofCountries["England"]);
+            }
+            else 
+            {
+                Console.WriteLine("This key does not exist");
+            }
+        }
+    }
+}
